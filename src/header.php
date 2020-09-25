4ad8361fa8849e3ad095a047110d641e7909e133
MediaBay <med.bay@yandex.ru>
2020-09-25 16:40:55 +0300

    favicons

--- src/header.php
+++ src/header.php
@@ -24,7 +24,14 @@
   <link rel="preload" href="<?php echo $template_directory . '/fonts/' . $font ?>" as="font" type="font/woff" crossorigin="anonymous" /> <?php
   endforeach ?>
   
-  <!-- favicons --> <?php
+  <!-- favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#616161">
+  <meta name="theme-color" content="#ffffff"> <?php
   wp_head() ?>
 </head>
 
